cb828ff9f8354b66d11f691e72fb7616457a40ca
umesh raut <uraut6044@gmail.com>
2024-10-04 14:26:52 +0530

    ui

--- landingpage/src/App.jsx
+++ landingpage/src/App.jsx
@@ -8,35 +8,35 @@ function App() {
 
   return (
     <>
-    <div className='mt-0 mb-0 w-full h-full fixed top-0 bottom-0 left-0 right-0'>
+    <div className='fixed top-0 bottom-0 left-0 right-0 w-full h-full mt-0 mb-0 '>
       <header className='flex flex-col'>
-        <nav className='p-3 bg-orange-700 h-12 w-full m-4 rounded-lg fixed top-0 left-0 bot\ right-0'>
-          <a className='mt-6 mb-6 p-5 text-white hover:text-black uppercase decoration-solid'>home</a>
-          <a className='mt-6 mb-6 p-5 text-white hover:text-black uppercase decoration-solid'>about</a>
-          <a className='mt-6 mb-6 p-5 text-white hover:text-black uppercase decoration-solid'>contact</a>
-          <a className='mt-6 mb-6 p-5 text-white hover:text-black uppercase font decoration-solid'>blogs</a>
+        <nav className='fixed top-0 bottom-0 left-0 right-0 w-full h-12 p-3 m-4 bg-orange-700 rounded-lg'>
+          <a className='p-5 mt-6 mb-6 text-white uppercase hover:text-black decoration-solid'>home</a>
+          <a className='p-5 mt-6 mb-6 text-white uppercase hover:text-black decoration-solid'>about</a>
+          <a className='p-5 mt-6 mb-6 text-white uppercase hover:text-black decoration-solid'>contact</a>
+          <a className='p-5 mt-6 mb-6 text-white uppercase hover:text-black font decoration-solid'>blogs</a>
         </nav>
       </header>
     </div>
     <div>
-      <main>
-        <div>
-          <h2>Hi there...</h2>
-          <h2>I am Your Full Stack Developer</h2>
-          <h3><p>I design and develope multiple large application for top giant companies and 
+      <main className='flex flex-col items-center w-full h-full'>
+        <div className='p-10 m-10'>
+          <h2 className='italic font-bold capitalize'>Hi there...</h2>
+          <h2 className='italic font-bold capitalize'>I am Your Full Stack Developer</h2>
+          <h3 className='italic font-bold capitalize'><p>I design and develope multiple large application for top giant companies and 
           i am very happy to say you are our next happy client guaranteed !
           </p></h3>
         </div>
         <div>
-          <img src='https://media.istockphoto.com/id/1438133166/photo/confident-mature-business-man-with-arms-crossed-looking-at-camera-smiling-standing-in-modern.jpg?s=612x612&w=0&k=20&c=a-SfGhlYEqjA9rTg3HELBdnjShXh5HN2ANAStur7xyo='></img>
+          <img className='m-5 rounded-xl size-100' src='https://media.istockphoto.com/id/1438133166/photo/confident-mature-business-man-with-arms-crossed-looking-at-camera-smiling-standing-in-modern.jpg?s=612x612&w=0&k=20&c=a-SfGhlYEqjA9rTg3HELBdnjShXh5HN2ANAStur7xyo='></img>
         </div>
 
-          <div>
-            <img src='https://img.freepik.com/premium-vector/stunning-embossed-yellow-coding-icon_462839-5599.jpg?size=626&ext=jpg' ></img>
-            <img src='https://img.freepik.com/premium-vector/stunning-embossed-yellow-coding-icon_462839-5599.jpg?size=626&ext=jpg' ></img>
-            <img src='https://img.freepik.com/premium-vector/stunning-embossed-yellow-coding-icon_462839-5599.jpg?size=626&ext=jpg' ></img>
-            <img src='https://img.freepik.com/premium-vector/stunning-embossed-yellow-coding-icon_462839-5599.jpg?size=626&ext=jpg' ></img>
-            <img src='https://img.freepik.com/premium-vector/stunning-embossed-yellow-coding-icon_462839-5599.jpg?size=626&ext=jpg' ></img>
+          <div className='item-center'>
+            <img className='m-5 border-2 rounded-xl size-80 ' src='https://img.freepik.com/premium-vector/stunning-embossed-yellow-coding-icon_462839-5599.jpg?size=626&ext=jpg' ></img>
+            <img className='m-5 border-2 rounded-xl size-80 ' src='https://img.freepik.com/premium-vector/stunning-embossed-yellow-coding-icon_462839-5599.jpg?size=626&ext=jpg' ></img>
+            <img className='m-5 border-2 rounded-xl size-80 ' src='https://img.freepik.com/premium-vector/stunning-embossed-yellow-coding-icon_462839-5599.jpg?size=626&ext=jpg' ></img>
+            <img className='m-5 border-2 rounded-xl size-80 ' src='https://img.freepik.com/premium-vector/stunning-embossed-yellow-coding-icon_462839-5599.jpg?size=626&ext=jpg' ></img>
+            <img className='m-5 border-2 rounded-xl size-80 ' src='https://img.freepik.com/premium-vector/stunning-embossed-yellow-coding-icon_462839-5599.jpg?size=626&ext=jpg' ></img>
           </div>
           
           <div>
